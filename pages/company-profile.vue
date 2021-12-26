97c760e61c906eb6ab33761c85fb24b91607b29e
Sadeq Naderi <sadeq.naderi@gmail.com>
2021-12-26 21:08:17 +0330

    Some update in company profile form

--- pages/company-profile.vue
+++ pages/company-profile.vue
@@ -20,29 +20,29 @@
 
 <script>
 /* eslint-disable */
-import SideBar from '../components/company/side-bar'
-import ProfileCard from '../components/company/profile-card'
-import Stepper from '../components/company/stepper'
-import Navigation from "~/components/navigation"
-export default {
-  components: {
-    SideBar,
-    ProfileCard,
-    Stepper,
-    Navigation
-  },
-  data() {
-    return {
-      drawer: null,
-      items: [
-        {name: 'My Inquiries', data: ['Add a New Buying Lead', 'Add a New Buying Lead', 'Add a New Buying Lead']},
-        {name: 'My Storefront', data: ['Add a New Buying Lead']},
-        {name: 'My Buying Tools', data: ['Add a New Buying Lead', 'Add a New Buying Lead', 'Add a New Buying Lead', 'Add a New Buying Lead']},
-        {name: 'My Selling Tools', data: ['Add a New Buying Lead', 'Add a New Buying Lead']},
-        {name: 'My Products', data: ['Add a New Buying Lead', 'Add a New Buying Lead', 'Add a New Buying Lead']},
-        {name: 'My account', data: ['Add a New Buying Lead']}
-      ],
+  import SideBar from '../components/company/side-bar'
+  import ProfileCard from '../components/company/profile-card'
+  import Stepper from '../components/company/stepper'
+  import Navigation from "~/components/navigation"
+  export default {
+    components: {
+      SideBar,
+      ProfileCard,
+      Stepper,
+      Navigation
+    },
+    data() {
+      return {
+        drawer: null,
+        items: [
+          {name: 'My Inquiries', data: ['Add a New Buying Lead', 'Add a New Buying Lead', 'Add a New Buying Lead']},
+          {name: 'My Storefront', data: ['Add a New Buying Lead']},
+          {name: 'My Buying Tools', data: ['Add a New Buying Lead', 'Add a New Buying Lead', 'Add a New Buying Lead', 'Add a New Buying Lead']},
+          {name: 'My Selling Tools', data: ['Add a New Buying Lead', 'Add a New Buying Lead']},
+          {name: 'My Products', data: ['Add a New Buying Lead', 'Add a New Buying Lead', 'Add a New Buying Lead']},
+          {name: 'My account', data: ['Add a New Buying Lead']}
+        ],
+      }
     }
   }
-}
 </script>
