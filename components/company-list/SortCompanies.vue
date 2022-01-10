5fe3c9db45421e0baa9ff05d01c46a980ac0f734
Sadeq Naderi <sadeq.naderi@gmail.com>
2022-01-10 21:27:55 +0330

    Enable ui of company list and company details

---
+++ components/company-list/SortCompanies.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="sort-companies">
+    <v-row class="d-flex items-center">
+      <v-col cols="auto" class="px-0">
+        <v-checkbox
+          v-model="enabled"
+          hide-details
+          class="shrink mr-2 mt-0"
+          label="Supplier"
+          hide-details
+        ></v-checkbox>
+      </v-col>
+      <v-col cols="auto" class="px-0">
+        <v-checkbox
+          v-model="enabled"
+          hide-details
+          class="shrink mr-2 mt-0"
+          label="Trade Assurance"
+          hide-details
+        ></v-checkbox>
+      </v-col>
+      <v-col cols="4" lg="2" class="pt-1 px-0">
+        <v-select
+          :items="items"
+          label="SortBy"
+          outlined
+          dense
+          hide-details
+        ></v-select>
+      </v-col>
+
+    </v-row>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "SortCompanies",
+    data() {
+      return {
+        items: ['Best Match', 'Ascending', 'descending', 'all'],
+      }
+    }
+  }
+</script>
+<style scoped>
+  .sort-companies{
+    border-bottom: 1px solid #eee;
+    padding: 10px;
+  }
+</style>
