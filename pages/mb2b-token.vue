5355afed1ebf3e17e0c4dc4c4665ad881067a889
Sadeq Naderi <sadeq.naderi@gmail.com>
2024-12-26 16:28:15 +0330

    Some update on style

--- pages/mb2b-token.vue
+++ pages/mb2b-token.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container>
     <v-row>
-      <v-col cols="12" md="12" class="mb-2">
+      <v-col cols="12" md="12" lg="12" class="mb-2">
         <h1>MB2B Token</h1>
       </v-col>
       <v-col cols="12">
