d912c9080b6750894582af30d965e4f176296681
Sadeq Naderi <sadeq.naderi@gmail.com>
2025-01-04 22:59:50 +0330

    Update product description preview

--- components/company-details/company-info.vue
+++ components/company-details/company-info.vue
@@ -1,24 +1,51 @@
 <template>
   <div class="py-8 company-details-info">
-    <div class="d-flex align-center justify-space-between flex-wrap company-info-details">
+    <div
+      class="d-flex align-center justify-space-between flex-wrap company-info-details"
+    >
       <div class="d-flex align-center flex-wrap pe-sm-16">
         <span v-if="logo === ''" class="me-4 empty-logo">&nbsp;</span>
         <div v-else-if="logo !== ''" class="logo-container mx-4">
-          <nuxt-img format="webp"
-                    :src="logo" :alt="(companyInfo.company_name ? companyInfo.company_name : 'No name')"/>
+          <nuxt-img
+            format="webp"
+            :src="logo"
+            :alt="
+              companyInfo.company_name ? companyInfo.company_name : 'No name'
+            "
+          />
         </div>
         <div class="company-name-info">
-          <p class="company-name">{{ companyInfo.company_name ? companyInfo.company_name : 'No name' }}</p>
+          <p class="company-name">
+            {{
+              companyInfo.company_name ? companyInfo.company_name : "No name"
+            }}
+          </p>
           <p class="company-slogan">{{ companySlogan }}</p>
         </div>
       </div>
-      <div class="contact-info" v-if="companyInfo.phone || companyInfo.owner.mobile_num">
+      <div
+        class="contact-info"
+        v-if="companyInfo.phone || companyInfo.owner.mobile_num"
+      >
         <div class="phone">
           <span class="icon">
             <v-icon>mdi-phone</v-icon>
           </span>
-          <a :href="'tel:'+companyInfo.phone ? companyInfo.phone : companyInfo.owner.mobile_num" target="_blank" class="number">
-            {{ companyInfo.phone ? companyInfo.phone : companyInfo.owner.mobile_num }}</a>
+          <a
+            :href="
+              'tel:' + companyInfo.phone
+                ? companyInfo.phone
+                : companyInfo.owner.mobile_num
+            "
+            target="_blank"
+            class="number"
+          >
+            {{
+              companyInfo.phone
+                ? companyInfo.phone
+                : companyInfo.owner.mobile_num
+            }}</a
+          >
         </div>
       </div>
     </div>
@@ -32,19 +59,20 @@ export default {
   },
   data() {
     return {
-      logo: '',
-      companySlogan: 'World class B2B wholesale platform',
-    }
+      logo: "",
+      companySlogan: "World class B2B wholesale platform",
+    };
   },
   mounted() {
     if (this.companyInfo.logo)
-      this.logo = "/storage/companies/" + this.companyInfo._id + "/" + this.companyInfo.logo;
-    else
-      this.logo = '';
-  }
-}
+      this.logo =
+        "/storage/companies/" +
+        this.companyInfo._id +
+        "/" +
+        this.companyInfo.logo;
+    else this.logo = "";
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
--- components/detail/Description.vue
+++ components/detail/Description.vue
@@ -1,28 +1,30 @@
 <template>
-  <v-col style="background-color: #f3f3f3;height: 100%;overflow:auto;" class="rounded-lg">
+  <v-col
+    style="background-color: #f3f3f3; height: 100%; overflow: auto"
+    class="rounded-lg"
+  >
     <v-row justify="space-between" class="py-4 px-3">
-      <v-col class="font-weight-black"> {{cardTitle}} </v-col>
+      <v-col class="font-weight-black"> {{ cardTitle }} </v-col>
       <!--      If Have Rate prop-->
-
     </v-row>
     <v-divider></v-divider>
     <!--    If have description prop  -->
     <v-col>
-      <div class="text-grey text-justify" v-if="productInfo && productInfo.details" v-html="productInfo.details">
-      </div>
+      <div
+        style="white-space: pre"
+        class="text-grey text-justify"
+        v-if="productInfo && productInfo.details"
+        v-html="productInfo.details"
+      ></div>
     </v-col>
   </v-col>
 </template>
 
 <script>
-
 export default {
-  props:['productInfo','cardTitle'],
+  props: ["productInfo", "cardTitle"],
   data() {
-    return {
-
-    };
+    return {};
   },
 };
-
 </script>
